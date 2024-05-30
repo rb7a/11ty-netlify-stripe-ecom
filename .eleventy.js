e4401fe2146fc0f2e07d62eca39554d9c83e23e2
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2024-05-30 14:09:24 +0100

    Add stripe and site input and output dir config

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("_includes/style.css");
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
+  }
 };
