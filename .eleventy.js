42ec159588d88f9fcfef222fd82b33f293c202b6
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2024-05-30 17:42:47 +0100

    more config changes to fix deploy errors

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,36 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("_includes/style.css");
+// const fs = require("fs");
+// const pluginRss = require("@11ty/eleventy-plugin-rss");
+// const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+// const filters = require("./src/_11ty/filters");
+// const shortcodes = require("./src/_11ty/shortcodes");
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("./src/bundle.js");
+
+  // Filters
+  Object.keys(filters).forEach((filterName) => {
+    eleventyConfig.addFilter(filterName, filters[filterName]);
+  });
+
   return {
+    templateFormats: ["md", "njk", "html", "liquid"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
     dir: {
-      input: "src",
-      output: "_site"
-    }
-  }
+      input: "./src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
 };
